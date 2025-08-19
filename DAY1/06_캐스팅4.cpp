249b6f4a14c913160c240fa2643b68d3551d325a
ecourse <smkang@codenuri.co.kr>
2025-08-19 15:29:24 +0900

    Add files via upload

---
+++ DAY1/06_캐스팅4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+// const_cast : 
+
+int main()
+{
+	const int c = 10;
+
+	int* p1 = (int*)&c; // ?
+
+	*p1 = 20;
+
+	// 아래 코드 결과 예측해 보세요???
+	std::cout << c   << std::endl; // ?
+	std::cout << *p1 << std::endl; // ?
+}
