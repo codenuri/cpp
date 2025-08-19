6e3504fdd0e14c21f46263e91e3c716ab6c95b82
ecourse <smkang@codenuri.co.kr>
2025-08-19 17:01:43 +0900

    Add files via upload

---
+++ DAY1/08_메모리할당.cpp
@@ -0,0 +1,17 @@
+﻿// 2_메모리할당 - 47 page
+#include <iostream>
+
+int main()
+{
+	// C 스타일
+	int* p = (int*)malloc(sizeof(int) * 20);
+	free(p);
+
+	// C++ 스타일
+	int* p1 = new int;
+	delete p1;
+
+	int* p2 = new int[10];
+	delete[] p2;
+}
+
