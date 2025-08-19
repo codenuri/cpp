a81172cde2c3a1a6dc809ca0aae0e28aa4659e76
ecourse <smkang@codenuri.co.kr>
2025-08-19 14:43:43 +0900

    Add files via upload

---
+++ DAY1/05_반복문.cpp
@@ -0,0 +1,19 @@
+﻿// 44page~
+#include <iostream>
+
+int main()
+{
+	int x[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	// C 언어의 전통적인 for 
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << x[i] << std::endl;
+	}
+	
+	// C++11 에서 추가된 새로운 for 문
+	for( int e : x )
+	{
+		std::cout << e << std::endl;
+	}
+}
