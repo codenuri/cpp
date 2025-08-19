f853ea3ed6d069d86083cdbe78d3a0997466906c
ecourse <smkang@codenuri.co.kr>
2025-08-19 16:49:25 +0900

    Add files via upload

--- DAY1/07_레퍼런스3-1.cpp
+++ DAY1/07_레퍼런스3-1.cpp
@@ -24,3 +24,24 @@ int main()
 	f1(a);
 	f2(a);
 }
+
+// 핵심 정리
+
+// 1. 인자값을 수정하고 싶다.
+// => 포인터 또는 레퍼런스로 해야 합니다.
+// => 레퍼런스가 안전합니다. "권장!!"
+void inc(int* p) {} // p != nullptr 을 확인해야 합니다.
+void inc(int& r) {} // r != nullptr 코드 필요 없음. 위 보다 안전
+
+// 2. 인자값을 수정하지 않아야 한다.
+// A. int, double 등의 primitive type => call by value 권장
+void f1(int n)        {int k = n;} // good
+void f2(const int& n) {int k = n;} // bad, n을 사용하는 것은 포인터 를 사용하는것
+									// 메모리를 따라가야(dereferencing) 합니다.
+
+// B. 인자가 구조체 등으로 만들어서 크기가 큰경우
+void f1(BigSizeType rc) {}		   // bad
+void f2(const BigSizeType& rc) {}  // good
+
+// Rect 정도(16byte)는 call by value 도 나쁘지는 않은데.. 보통은 const &
+// => 8 바이트 정도 까지는 call by value 권장
