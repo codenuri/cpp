5737ac0d9252cc935eef8e6c1ff82b73470ad839
ecourse <smkang@codenuri.co.kr>
2025-08-19 11:18:25 +0900

    Add files via upload

---
+++ DAY1/01_namespace4.cpp
@@ -0,0 +1,22 @@
+﻿// 10 page 내용입니다.
+
+//#include <stdio.h>  // C언어용 헤더. printf 등이 global 영역에만 있습니다.
+#include <cstdio>     // C++언어가 다시 만든 헤더
+					  // 모든 C 함수가 std:: 안에도 있습니다.
+
+// C언어용 헤더     C++이 새롭게 만든헤더
+// <xxxx.h>       <cxxxx>
+// <stdio.h>  ==> <cstdio>
+// <stdlib.h> ==> <cstdlib>
+// <math.h>   ==> < ? >
+// <string.h> ==> < ? >
+
+int main()
+{
+	// #1. 모든 C++ 표준은 std 이름 공간에 있습니다.
+	// #2. printf 는 C 표준이지만, C++ 표준이기도 합니다.
+
+	printf("hello\n"); // ok
+
+	std::printf("hello\n"); // ??
+}
