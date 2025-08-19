51b4d71cbda1d58fb97b6dc7f755d6ace99a487a
ecourse <smkang@codenuri.co.kr>
2025-08-19 14:46:40 +0900

    Add files via upload

--- DAY1/05_반복문.cpp
+++ DAY1/05_반복문.cpp
@@ -10,10 +10,19 @@ int main()
 	{
 		std::cout << x[i] << std::endl;
 	}
-	
-	// C++11 에서 추가된 새로운 for 문
+
+	// C++11 에서 추가된 새로운 for 문(range-for)
+	// 파이썬 : for e in x
+	// C#    : foreach e in x
+	// 
 	for( int e : x )
 	{
 		std::cout << e << std::endl;
 	}
+
+	// 전체가 아닌 5개만 할수 있나요 ?
+	// 1칸이 아닌 2칸씩 이동되나요 ?
+	// 거꾸로 할수 없나요 ?
+	// => 모두 C++20 부터 가능합니다.
+	// => 그전에는 "C style for" 문 사용하세요. 
 }
