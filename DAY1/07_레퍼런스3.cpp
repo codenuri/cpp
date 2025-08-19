03f4ab04592c858e328a6c89e59d7ddf6937e603
ecourse <smkang@codenuri.co.kr>
2025-08-19 16:35:41 +0900

    Add files via upload

--- DAY1/07_레퍼런스3.cpp
+++ DAY1/07_레퍼런스3.cpp
@@ -8,9 +8,14 @@ void foo(int n) // call by value : 인자의 원본값을 절때 변경하지 �
 	n = 3;
 }
 
-void goo(Rect rc) // call by value : 
-{
+//void goo(Rect rc) // call by value : 원본을 수정할수 없으므로 안전
+				  //				 하지만, 복사본 생성에 대한 메모리 오버헤드 존재
 
+void goo(const Rect& rc)  // call by const reference : 원본 수정이 안되어서 안전				   
+{						  //				복사본도 없으므로 메모리 오버헤드도 없다
+						  //                C++ 에서 가장 널리 사용하는 인자 전달방식
+	
+//	rc.x = 10; // error. 상수이므로 변경 불가
 }
 
 int main()
