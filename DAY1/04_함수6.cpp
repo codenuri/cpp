1d5757550f887ee3c7fc7a59c2ac82fbfc1cd8c7
ecourse <smkang@codenuri.co.kr>
2025-08-19 17:25:39 +0900

    Add files via upload

--- DAY1/04_함수6.cpp
+++ DAY1/04_함수6.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+// 아래 처럼만 하면 int 2개만 가능
+/*
 int add(int a, int b)
+{
+	return a + b;
+}
+*/
+
+// 아래 처럼하면 모든 타입에 대해서 함수 생성가능
+template<typename T>
+T add(T a, T b)
 {
 	return a + b;
 }
