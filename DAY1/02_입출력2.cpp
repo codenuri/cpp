18e419ce06abd9fe7d735bae1b03bf703a746e99
ecourse <smkang@codenuri.co.kr>
2025-08-19 11:31:11 +0900

    Add files via upload

---
+++ DAY1/02_입출력2.cpp
@@ -0,0 +1,16 @@
+﻿// 3_입출력2.cpp - 18 page
+#include <iostream>
+
+int main()
+{
+	int n = 10;
+
+	std::cout << n << std::endl; // 10 즉, 10진수 출력
+
+	std::cout << std::hex << n << std::endl; // a,  16진수 출력
+
+	std::cout << std::dec << n << std::endl; // 
+
+	
+	std::cout << std::setw(10) << set::setfill('#') <<  n << std::endl; // 
+}
