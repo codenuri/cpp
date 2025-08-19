e36a2b89a7c36028afe4d884f196e505c8a09ec3
ecourse <smkang@codenuri.co.kr>
2025-08-19 17:16:45 +0900

    Add files via upload

--- DAY1/04_함수5.cpp
+++ DAY1/04_함수5.cpp
@@ -1,12 +1,21 @@
 // 04_함수5.cpp
 
 // C++98 스타일의 함수
-int square(int a)
+int square1(int a)
+{
+	return a * a;
+}
+
+// C++11 에서 추가된 새로운 스타일의 함수
+// => 핵심 : 반환 타입을 뒤에 표기.
+// => suffix return type 이라는 문법
+// => 아래 코드는 위와 동일
+auto square2(int a) -> int
 {
 	return a * a;
 }
 
 int main()
 {
-	
+
 }
