feaf657f133a9fc788136111df39f94b0e4c94b9
ecourse <smkang@codenuri.co.kr>
2025-08-19 11:11:00 +0900

    Add files via upload

---
+++ DAY1/01_namespace3.cpp
@@ -0,0 +1,17 @@
+﻿// 1_namespace3.cpp - 9page 내용
+// 
+#include <algorithm>
+using namespace std;
+
+// 핵심 : C++의 모든 표준 함수는 "std" 라는 namespace 안에 있습니다.
+
+int count = 0; // 사용자가 만든 전역변수
+
+int main()
+{
+	int n = min(3, 2); // C++ 표준 함수. algorithm 헤더 필요
+					   // std namespace 안에 있습니다.
+
+	int ret = count; // 이 코드가 왜??? 에러 일까요 ???
+					 // 어떤 문제가 있을까요 ??
+}
