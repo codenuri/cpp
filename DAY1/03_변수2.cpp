8f00962d40c94ecdb2ac73e7c2a4eefc008e475c
ecourse <smkang@codenuri.co.kr>
2025-08-19 13:16:22 +0900

    Add files via upload

--- DAY1/03_변수2.cpp
+++ DAY1/03_변수2.cpp
@@ -24,8 +24,25 @@ int main()
 	int x3[3]{ 1,2,3 };
 	Point p3 { 1,2 };	
 
-	// = 있는 초기화 : copy initialization
+	// = 있는 초기화 : copy   initialization
 	// = 없는 초기화 : direct initialization
+	//    => 약간의 차이가 있는데, 현재 위 코드는 동일, 
+	//    => 사용자 정의 타입(class)일때만 차이 있음. 
+	// ------------------------------------------
+	// prevent narrow
+
+	int n4 = 3.4; // 실수 => 정수 변수에 넣기. 데이타 손실 발생. 
+				  // C/C++ 은 가능. 하지만 대부분의 언어는 에러.!
+
+	char c1 = 300; // 에러는 아니지만 overflow 발생!!
+
+	int  n5 = {3.4}; // error. 데이타 손실이 있으면 에러
+	char c2 = {300}; // error. overflow 가 있어도 에러
+	char c3 {300};   // 역시 error.
+	
+	// 결론 : 안전한 프로그램을 원한다면 {} 초기화를 사용하세요. 
+	// MISRA-C++, autosar C++ : 안전한 코드를 위한 규칙을 정리 한것 
+
 }
 
 
