8e3a3a4e2743425c7b4a771a6dfce08f2b323d7d
ecourse <smkang@codenuri.co.kr>
2025-08-19 11:21:36 +0900

    Add files via upload

--- DAY1/01_namespace4.cpp
+++ DAY1/01_namespace4.cpp
@@ -2,14 +2,18 @@
 
 //#include <stdio.h>  // C언어용 헤더. printf 등이 global 영역에만 있습니다.
 #include <cstdio>     // C++언어가 다시 만든 헤더
-					  // 모든 C 함수가 std:: 안에도 있습니다.
+					  // 모든 C 함수가 global 과 std:: 안에 모두 있습니다.
 
 // C언어용 헤더     C++이 새롭게 만든헤더
 // <xxxx.h>       <cxxxx>
 // <stdio.h>  ==> <cstdio>
 // <stdlib.h> ==> <cstdlib>
-// <math.h>   ==> < ? >
-// <string.h> ==> < ? >
+// <math.h>   ==> <cmath >
+// <string.h> ==> <cstring>
+
+// C++언어
+// => 대부분의 C 언어용헤더 그대로 사용가능
+// => 하지만, C++이 새롭게 만든 헤더 사용 권장
 
 int main()
 {
