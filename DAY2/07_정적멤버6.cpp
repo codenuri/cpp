4fd00aec9dde7fb318d5003e6ece00adc8dbd7a3
ecourse <smkang@codenuri.co.kr>
2025-08-20 14:28:08 +0900

    Add files via upload

---
+++ DAY2/07_정적멤버6.cpp
@@ -0,0 +1,20 @@
+// 07_정적멤버6.cpp
+#include <iostream>
+
+// 예제!
+
+// 프로그램에서 날짜를 많이 사용한다.
+// => "Date" 라는 타입을 만들자
+
+class Date 
+{
+public:
+	int year;
+	int month;
+	int day;
+};
+
+int main()
+{
+	
+}
