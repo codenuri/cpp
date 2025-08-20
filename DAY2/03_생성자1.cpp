0fd440573e06a0cf6f6f8f536282cb6ecbc26fae
ecourse <smkang@codenuri.co.kr>
2025-08-20 11:11:53 +0900

    Add files via upload

--- DAY2/03_생성자1.cpp
+++ DAY2/03_생성자1.cpp
@@ -8,11 +8,23 @@ private:
 	int speed;
 
 public:	
+/*
 	void init(int c, int s)
 	{
 		color = c;
 		speed = s;
 	}
+*/
+	// 생성자
+	// 1. 클래스 이름과 동일한 함수
+	// 2. 반환값이 없고, 반환 타입도표기하지 않는다.
+	// 3. 객체를 생성하면 자동으로 호출된다.
+	// 필요성 : private 의 멤버 데이타 초기화를 위해 사용
+	Car(int c, int s)
+	{
+		color = c;
+		speed = s;
+	}	
 };
 
 int main()
@@ -23,8 +35,11 @@ int main()
 	// Car c = { 10, 80};
 
 	// #1. 객체를 생성후, 멤버 함수를 사용해서 초기화
-	Car c;
-	c.init();
+	// Car c;
+	// c.init(1, 80);
+
+	// #2. 생성자를 사용한 초기화
+	Car c(1, 80);
 
 }
 
