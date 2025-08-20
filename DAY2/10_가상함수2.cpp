11873027b82592ed1f56da83118dc94f7de11cb6
ecourse <smkang@codenuri.co.kr>
2025-08-20 17:28:15 +0900

    Add files via upload

---
+++ DAY2/10_가상함수2.cpp
@@ -0,0 +1,26 @@
+﻿// 가상함수 원리
+// => 복잡한 내용이지만
+// => 임베디드 개발자는 "필수"
+// => "메모리 오버헤드를 알아야 합니다."
+
+class Base 
+{
+	int a;
+public:
+	virtual void f1() {}
+	virtual void f2() {}
+	virtual void f3() {}
+};
+class Derived : public Base 
+{
+	int b;
+public:
+	virtual void f2() {}
+};
+int main()
+{
+	Base b1, b2;
+	Derived d;
+	Base* p = &d;
+	p->f2();
+}
