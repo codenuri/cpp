f0f3abf6be756eb1711724eb97e4f51b8c963344
ecourse <smkang@codenuri.co.kr>
2025-08-20 14:02:42 +0900

    Add files via upload

---
+++ DAY2/07_정적멤버1.cpp
@@ -0,0 +1,24 @@
+﻿// 3_정적멤버1 - 110 page
+#include <iostream>
+
+// Car 객체를 몇개나 생성했는지 갯수를 알고 싶다
+
+class Car
+{
+	int color;
+	int speed;
+public:
+	int cnt = 0;
+
+	Car()  { ++cnt; }
+	~Car() { --cnt; }
+};
+
+int main()
+{
+	Car c1;
+	Car c2;
+
+	std::cout << c1.cnt << std::endl;
+}
+
