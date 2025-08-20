3928c116d09419248d9bdd0d3270a86ebc72c9d4
ecourse <smkang@codenuri.co.kr>
2025-08-20 15:32:58 +0900

    Add files via upload

---
+++ DAY2/08_상속1.cpp
@@ -0,0 +1,27 @@
+﻿// 5_상속1.cpp   133page ~
+#include <iostream>
+
+class Person
+{
+	char name[32];
+	int  age;
+};
+// Person 을 확장해서 새로운 타입을 설계 합니다.
+// => 상속 문법
+// Person  : Base 클래스
+// Student : Derived 클래스 라는 용어 사용
+class Student : public Person
+{	
+	int    id;
+};
+
+class Professor: public Person
+{
+	int    major;
+};
+
+int main()
+{
+	Student s;
+	Professor p;
+}
