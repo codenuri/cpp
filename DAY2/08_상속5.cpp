09ffc4fb0f728758abc7b7c37a4a9b67f8f996d3
ecourse <smkang@codenuri.co.kr>
2025-08-20 16:09:21 +0900

    Add files via upload

--- DAY2/08_상속5.cpp
+++ DAY2/08_상속5.cpp
@@ -7,8 +7,13 @@ class People
 {
 	int age;
 public:
-	People(int age) : age{age} {}
+	People(int age) : age{age} {   }
 };
+// 1. People 로 부터 상속받은 Student 를 만들어 보세요
+// 2. Student 에 int id 멤버 데이타 추가해 보세요
+// 3. Student 에 생성자 추가해 보세요
+// 4. main 에서 Student 객체 생성해 보세요
+
 
 int main()
 {
@@ -17,9 +22,6 @@ int main()
 
 
 
-// 1. People 로 부터 상속받은 Student 를 만들어 보세요
-// 2. Student 에 int id 멤버 데이타 추가해 보세요
-// 3. Student 에 생성자 추가해 보세요
-// 4. main 에서 Student 객체 생성해 보세요
+
 
 
