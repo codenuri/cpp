b3303001ed3f5fde9c4d7890e938259072b27ee5
ecourse <smkang@codenuri.co.kr>
2025-08-20 11:43:10 +0900

    Add files via upload

--- DAY2/04_초기화리스트3.cpp
+++ DAY2/04_초기화리스트3.cpp
@@ -3,11 +3,14 @@
 class Point
 {
 	int x, y;
+	const int c;  // 상수는 반드시 초기화 되어야 합니다.
 public:
-	Point(int a, int b) : x(a), y(b)  
+	Point(int a, int b) : x(a), y(b), c(0)     // <= 이위치는 초기화 입니다.
 	{
-		x = a; 
-		y = b; 
+		// 아래 2줄은 대입니다.
+		// x = a; 
+		// y = b; 
+	//	c = 0; // error. 이 위치는 대입 입니다.
 	}
 };
 
