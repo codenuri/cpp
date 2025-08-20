ef3f46fc139b1ecb5437e3c01a0320f9b639789a
ecourse <smkang@codenuri.co.kr>
2025-08-20 14:33:55 +0900

    Add files via upload

--- DAY2/07_정적멤버6.cpp
+++ DAY2/07_정적멤버6.cpp
@@ -8,13 +8,28 @@
 
 class Date 
 {
-public:
 	int year;
 	int month;
 	int day;
+public:
+	Date(int y, int m, int d) : year{y}, month{m}, day{d} 
+	{		
+	}
+	// getter/setter - 관례적인 코드
+	// 핵심 : getter 는 상수 멤버 함수이어야 한다.
+	void set_month(int m) 
+	{
+		if ( m >= 1 && m <= 12 )
+			month = m;
+	}
+	int get_month() const { return month; }
+
+	// year,day 의 setter/getter는  복습하실때 구현해 보세요.. 
 };
 
 int main()
 {
-	
+	Date today{2025, 8, 20};
+
+	auto m = today.get_month();
 }
