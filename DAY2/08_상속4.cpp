80051708d36e8ec119fc8c4a32d24739350fa11a
ecourse <smkang@codenuri.co.kr>
2025-08-20 15:52:54 +0900

    Add files via upload

--- DAY2/08_상속4.cpp
+++ DAY2/08_상속4.cpp
@@ -13,8 +13,15 @@ class Derived : public Base
 {
 public:
 	// 사용자 코드			 // 컴파일러가 변경한 코드
-	Derived() {}			// Derived()      : Base() {}
-	Derived(int a) {}		// Derived(int a) : Base() {}
+//	Derived() {}			// Derived()      : Base() {}
+//	Derived(int a) {}		// Derived(int a) : Base() {}
+
+
+	// 해결책 
+	// 기반 클래스에 디폴트 생성자가 없다면
+	// 파생 클래스 생성자에서 반드시 기반 클래스의 다른 생성자를 호출하는 코드를 제공해야 합니다.
+	Derived()      : Base(0) {}
+	Derived(int a) : Base(a) {}
 };
 
 int main()
