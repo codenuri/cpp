da1ef4e3f8ae5dc4d1cc21dd3261530d39ead118
ecourse <smkang@codenuri.co.kr>
2025-08-20 11:29:43 +0900

    Add files via upload

---
+++ DAY2/04_초기화리스트1.cpp
@@ -0,0 +1,25 @@
+﻿// 6_초기화리스트1    89page ~
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	// member initializer list (멤버 초기화 리스트)
+	// => 생성자 () 뒤에 : 을 표기하고, 각 멤버를 초기화 하는 것.. 
+
+	Point(int a, int b) : x(a), y(b)  // 좋은 코드!!!
+	{
+//		x = a;  // 나쁜 코드!!
+//		y = b;  // 이렇게 하지 마세요
+	}
+};
+
+int main()
+{
+	Point pt(0, 0);
+}
+
+
+
+
