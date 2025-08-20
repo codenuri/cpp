150524bf7b8eb0a11b58760ed393ed9f0c27225b
ecourse <smkang@codenuri.co.kr>
2025-08-20 10:21:47 +0900

    Add files via upload

--- DAY2/01_OOP3.cpp
+++ DAY2/01_OOP3.cpp
@@ -7,6 +7,12 @@
 
 // 타입을 만들때
 // => 상태를 나타내는 데이타와 상태를 가지고 연산을 하는 함수를 묶어서 설계 한다.
+
+// 함수를 구조체 안에 넣어서 얻는 장점은 뭔가요 ?
+// => 함수 인자로 데이타를 별도로 전달할 필요 없어서 간결해지고
+// => 접근지정자, 상수 함수, static 등의 문법을 통해서 사용하기 쉽고 안전한 타입을 만들수
+//    있습니다.
+
 struct Rect 
 {
 	// 멤버 데이타
@@ -15,15 +21,14 @@ struct Rect
 	int right;
 	int bottom;
 	// 멤버 함수 - 멤버 함수 안에서는 멤버 데이타를 직접 접근 가능
-	int getRectArea()  { return (right - left) * (bottom - top); }
-	void drawRect()    { std::cout << "draw rect" << std::endl; }
+	int getArea()  { return (right - left) * (bottom - top); }
+	void draw()    { std::cout << "draw rect" << std::endl; }
 };
-
 int main()
 {
 	Rect rc = { 1, 1, 10, 10};
-	int n1 = rc.getRectArea();
-	rc.drawRect();
+	int n1 = rc.getArea();
+	rc.draw();
 
 //	int n1 = getRectArea(rc);
 //	drawRect(rc);
