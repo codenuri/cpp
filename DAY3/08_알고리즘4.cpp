f6632b272f3ebacc5defb6f89fb0c577f214030a
ecourse <smkang@codenuri.co.kr>
2025-08-21 15:20:48 +0900

    Add files via upload

--- DAY3/08_알고리즘4.cpp
+++ DAY3/08_알고리즘4.cpp
@@ -2,14 +2,23 @@
 #include <algorithm>
 #include <array>
 
+bool foo(int e) 
+{
+	return e % 3 == 0;
+}
 int main()
 {
-	std::array<int, 10> arr = { 1,2,3,4,5,6,7,8,9,10 };
+	std::array<int, 10> arr = { 1,2,6,4,5,3,7,8,9,10 };
+
 
 	// 주어진 구간에서 "3"을 찾고 싶다.
 	auto ret1 = std::find(arr.begin(), arr.end(), 3); 
 
 
 	// 주어진 구간에서 "3의 배수"을 찾고 싶다.
-	auto ret2 = std::find(arr.begin(), arr.end(),  ?); 	
+	auto ret2 = std::find_if(arr.begin(), arr.end(),  foo ); 	
+	std::cout << *ret2 << std::endl; // 6
+
+	// std::find   (first, last, 값)      : 값 검색
+	// std::find_if(first, last, 조건함수) : 조건을 만족하는 값 검색
 }
