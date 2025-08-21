ccd941546dd5198b627471cca89bb48855b5f1b9
ecourse <smkang@codenuri.co.kr>
2025-08-21 13:45:48 +0900

    Add files via upload

--- DAY3/sample4.cpp
+++ DAY3/sample4.cpp
@@ -32,6 +32,18 @@ public:
 		return (bool) rand() % 2;
 	}
 };
+//--------------------------------------
+// 주차장 상황이 변경될때 마다 Display 해야 하는데
+// 주차장 정문, 인터넷, 고객 sms 등 다양한 방식이 가능해야 합니다.
+
+struct IDisplay 
+{
+	virtual void display(int free_count, int first_free) = 0;
+	virtual void display_all( int* state) {} // 배열을 통채로 전달하는 함수
+											 // 전체상황 출력 가능하게
+											 // =0 이 아니므로 필요 한 경우만 구현
+	virtual ~IDisplay() {}
+};
 
 
 
@@ -40,11 +52,15 @@ class ParkingLot
 {
 private:
 	ISensor* sensor = nullptr; 
+	IDisplay* display[256]; // 최대 256개의 display 객체 연결가능
+	int display_cnt = 0;
 
 	bool state[MAX_SPOTS];
 
 public:
 	void set_sensor(ISensor* s) { sensor = s;}
+	void add_display(IDisplay* d) { display[display_cnt++] = d;}
+
 
 	bool read_sensor(int no)
 	{
