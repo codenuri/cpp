0408bb58b29a020bde023384a9ea6989f1b5f770
ecourse <smkang@codenuri.co.kr>
2025-08-21 10:06:08 +0900

    Add files via upload

---
+++ DAY3/01_example1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// 파워 포인트 값은 프로그램을 객체지향으로 만든다고 생각해 봅시다.
+
+// 1. 각각의 도형을 타입으로 설계 합니다.
+
+class Rect 
+{
+public:
+	void draw() { std::cout << "draw Rect\n"; }
+};
+
+class Circle 
+{
+public:
+	void draw() { std::cout << "draw Rect\n"; }
+};
+int main()
+{
+	Rect* arr[10];	
+}
