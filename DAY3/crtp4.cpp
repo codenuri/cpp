59e331a3ccc71552003a2155b39c408700300b48
ecourse <smkang@codenuri.co.kr>
2025-08-21 16:19:05 +0900

    Add files via upload

--- DAY3/crtp4.cpp
+++ DAY3/crtp4.cpp
@@ -21,17 +21,28 @@ public:
 	void write() {}
 	int  recv() {return ERROR; 	}
 
-	void run_loop()
+	void run_loop()			// void run_loop(uart* this)
 	{
 		int ret = recv();
 		
 		switch(ret)
 		{
-			case SUCCESS: on_receive(); break;
+			case SUCCESS: 
+				on_receive(); // 이 코드는 this->on_receive() 입니다.
+							  // 그런데 현재 this 는 uart* 입니다.
+							  // 그리고, on_receive() 는 가상함수가 아니기 때문에
+							  // static binding 하므로 포인터 타입에 따라 호출
+							  // 그래서, 객체는 MySerial 이지만 포인터 타입이  uart라서
+							  // uart 의 함수 호출
+							  // 결론 : 파생 클래스가 다시 만들었지만 기반 클래스 함수 호출
+							  // => 가상함수가 아니므로!
+				break;
+
 			case ERROR: on_error(); break;
 			case UNKNOWN: break;
 		}
 	}
+
 	void on_receive() { std::cout << "uart on_receive()\n";}
 	void on_error()   { std::cout << "uart on_error()\n";}
 };
