c5902c12c6a09446c1383f8f7c8680ba5fdf0612
ecourse <smkang@codenuri.co.kr>
2025-09-16 20:41:31 +0900

    Add files via upload

---
+++ DAY2/02_casting6.cpp
@@ -0,0 +1,17 @@
+﻿int main()
+{
+	const int c = 10;
+
+	// c의 주소를 double* 에 담고 싶습니다.
+	double* p1 = (double*)&c; // ok. C 스타일 캐스팅은 아주 강력!!
+
+	// 위 코드는 C++ 캐스팅으로 해보세요
+
+	double* p2 = ??;
+
+}
+
+
+
+
+
