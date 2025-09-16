b076fb14d63ce39a23fbbfcb146a3085a7156d5d
ecourse <smkang@codenuri.co.kr>
2025-09-16 20:06:58 +0900

    Add files via upload

---
+++ DAY2/02_casting1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+	int n = 3;
+
+//	double* p = &n; // error
+	double* p = (double*)&n; // ok
+
+	// 위 캐스팅이 성공했다면 아래 처럼사용가능합니다.
+	// => 아래 코드 실행시 메모리 그림을 생각해 보세요
+	*p = 3.4;
+
+}
