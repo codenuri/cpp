8eb26b6fd61716827aeec5a677f4e68996cd360a
ecourse <smkang@codenuri.co.kr>
2025-09-16 21:30:17 +0900

    Add files via upload

---
+++ DAY2/03_member_initializer_list3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+class People1 
+{
+	std::string name;
+	int age;
+public:
+	People1(const std::string& n, int a)
+	{
+		name = n;
+		age = a;
+	}
+};
+
+class People2
+{
+	std::string name;
+	int age;
+public:
+	People2(const std::string& n, int a) : name{n}, age{a}
+	{
+	}
+};
+
+int main()
+{
+	People1 p1{"kim", 20};
+	People2 p2{"kim", 20};
+}
+
+
+
+
