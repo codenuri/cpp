82eb29afac948b4f2ba0e445ad0f93117dc55d98
ecourse <smkang@codenuri.co.kr>
2025-09-16 21:03:38 +0900

    Add files via upload

---
+++ DAY2/03_member_initializer_list1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+class People 
+{
+	std::string name;
+	int age;
+public:
+	// member intializer list
+	// => 생성자 ()뒤에 : 을 표기하고 멤버 데이타를 초기화 하는 것
+	// => C++11 이후는 () 보다 {} 권장
+//	People(const std::string& n, int a) : name(n), age(a) // 좋은 코드
+	People(const std::string& n, int a) : name{n}, age{a} // 좋은 코드
+	{
+		// 아래 처럼 멤버를 초기화 하는 것은 "나쁜 코드"
+		// name = n;	// bad
+		// age = a;
+	}
+};
+
+int main()
+{
+//	People p("kim", 20); // 예전 스타일
+	People p{"kim", 20}; // C++11 이후 권장
+}
+
+
+
+
