a7cdd3c979d3ff8fb1c7083ef7c07936dafe77f3
ecourse <smkang@codenuri.co.kr>
2025-09-17 20:20:09 +0900

    Add files via upload

---
+++ DAY3/03_inheritance1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+
+// 사람의 공통의 특징을 담은 클래스를 먼저 설계하고
+class People 
+{
+	std::string name;
+	int age;
+};
+
+// People 을 확장해서 새로운 타입을 설계 합니다.
+class Student : public People
+{	
+	int    id;
+};
+class Professor: public People
+{
+	int    major;
+};
+
+int main()
+{
+	Student s;
+	Professor p;
+}
