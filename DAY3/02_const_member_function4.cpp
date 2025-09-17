1ba1230b42b959e2c47db8b11d4c1d64f14baa5c
ecourse <smkang@codenuri.co.kr>
2025-09-17 20:16:19 +0900

    Add files via upload

--- DAY3/02_const_member_function4.cpp
+++ DAY3/02_const_member_function4.cpp
@@ -6,8 +6,16 @@ class Point
 {
 	int x, y;
 
-	std::string cache;
-	bool cache_valid = false;
+	// mutable 
+	// => 상수 멤버 함수 에서도 값을 변경할수 있게 해달라는 의미
+	// => to_string() 은 논리적으로는 상수 멤버함수가 되는게 맞지만
+	//    다양한 테크닉을 위해서는 멤버의 값을 변경할 필요가 있다.
+	//    (논리적 상수성 이라는 문제)
+
+	// => 상수 멤버 함수를 사용하기 시작하면 많은 에러가 발생할수있습니다.
+	//    적절한 문법과 다양한 테크닉으로 해결해야 합니다.
+	mutable std::string cache;
+	mutable bool cache_valid = false;
 public:
 	Point(int a, int b) : x{a}, y{b} {}
 
