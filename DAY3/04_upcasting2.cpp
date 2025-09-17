69c1844eba0e1be66dab34e11f4f0f8c1f72eace
ecourse <smkang@codenuri.co.kr>
2025-09-17 21:18:46 +0900

    Add files via upload

--- DAY3/04_upcasting2.cpp
+++ DAY3/04_upcasting2.cpp
@@ -18,11 +18,28 @@ int main()
 
 	Animal* p = &a;
 
-	// #1. static_cast
+	// #1. static_cast : 컴파일 시간 캐스팅.
+	// => 1. 컴파일러는 컴파일 시간에는 p가 가리키는 곳에 어떤 객체가 있는지 알수 없습니다.
+	//       (실행시 변경될수도 있으므로)
+
+	// => 2. 따라서 static_cast 는 p가 가리키는 곳에 Dog가 있는지 조사할수는 없습니다.
+	// => 3. 그래서 아래 코드는 항상 성공하고 주소가 나옵니다.
+	// => 4. 즉, 아래 코드는 개발자가 책임지겠다는 의미 
+	// => 하지만 지금은 p가 가리키는 곳이 Dog가 아니므로 
+	//    pd->color = 10 같은 코드를 만들면 버그
 
 	Dog* pd = static_cast<Dog*>(p);
 
 	std::cout << pd << std::endl;
+
+
+	// #2. dynamic_cast : 실행시간 캐스팅
+	// => 실행시간에 p가 가리키는 곳을 조사해서 Dog일때만 주소 반환
+	// => Dog가 아니라면 0 반환
+
+	Dog* pd2 = dynamic_cast<Dog*>(p);
+
+	std::cout << pd2 << std::endl;
 }
 
 
