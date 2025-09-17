46b35e8d4c7dfe9d148b3ff7b4b55444c9320725
ecourse <smkang@codenuri.co.kr>
2025-09-17 19:12:24 +0900

    Add files via upload

--- DAY3/01_this1.cpp
+++ DAY3/01_this1.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+// 핵심 #1. 멤버 함수의 1번째 인자로 this 가 추가됩니다. - "this call" 이라고 합니다.
+// 핵심 #2. static 멤버 함수의 1번째 인자로 this 가 추가안됩니다.
 
 class Point
 {
@@ -14,9 +16,17 @@ public:
 		// => 현재 멤버 함수를 호출할때 사용한 객체의 주소
 		std::cout << this << std::endl;
 	}
+	static void foo(int a)	// void foo(int a) <= 컴파일 후에도 this 는 추가 안됨.
+	{
+		x = a;				// 		this->x = a; 로 변경되어야 하는데,,
+	}						//					 static 멤버 함수는 this 가 없어서 error
+							//                   그래서, static 멤버 함수에서는 
+							//				     멤버 데이타 접근 안됨. 
 };
 int main()
 {
+	Point::foo(3); // static 멤버 함수는 객체 없이 호출
+
 	Point p1;
 	Point p2;
 	std::cout << "p1 : " << &p1 << std::endl;
