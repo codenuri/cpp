6d9a26d38d5a626002b63c3ff279fa55f2a86964
ecourse <smkang@codenuri.co.kr>
2025-09-18 19:49:07 +0900

    Add files via upload

---
+++ DAY4/3_shape_example4.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+class Shape 
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
+{
+public:
+	void draw() { std::cout << "draw rect\n"; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() { std::cout << "draw circle\n"; }
+};
+
+int main()
+{
+	std::vector<Shape*> v; 
+
+	while( 1 )
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if 		( cmd == 1 ) v.push_back(new Rect);
+		else if ( cmd == 2 ) v.push_back(new Circle);
+		else if ( cmd == 9 )
+		{
+			for ( auto s : v)
+			{
+				// 해결책 #1. Shape* 인 s 를 Rect* 나 Circle* 로 캐스팅해서 사용
+
+				static_cast<Rect*>(s)->draw();		
+			}
+		}
+	}
+}
+/
