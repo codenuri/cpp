9a97be16b23d96b8a960013fa4e728d26ffc4433
ecourse <smkang@codenuri.co.kr>
2025-09-18 20:42:33 +0900

    Add files via upload

--- DAY4/3_shape_example8.cpp
+++ DAY4/3_shape_example8.cpp
@@ -16,7 +16,7 @@ public:
 	// 자신의 복사본을 만드는 가상함수는 아주 유용하게 사용됩니다.
 	virtual Shape* clone() const 
 	{
-		return new Shape(*this);
+		return new Shape(*this); // 복사 생성자로 생성, 즉 자신(*this) 와 동일한 객체 생성
 	}
 };
 
@@ -27,11 +27,17 @@ class Rect : public Shape
 {
 public:
 	void draw() override { std::cout << "draw rect\n"; }
+
+	Shape* clone() const override {	return new Rect(*this); }
 };
+
+
 class Circle : public Shape
 {
 public:
 	void draw() override { std::cout << "draw circle\n"; }
+
+	Shape* clone() const override {	return new Circle(*this); }
 };
 
 
@@ -57,7 +63,16 @@ int main()
 			int k;
 			std::cin >> k;
 
-
+			// k 번째 도형이 어떤 도형인지 조사할 필요 없습니다.
+			// => 단지 "복사본좀 만들어줘!!" 라고 요청만 하면 됩니다
+			v.push_back( v[k]->clone() ); 	// 다형성	
+											// 새로운 도형이 추가되어도 
+											// 이 코드는 수정될 필요 없습니다.
+											
+			// java 진영에서 널리 사용되는 용어 입니다. 
+			// 의미를 생략해 보세요
+			// "Don't Ask, Do It" 
+			// 물어보지 말고(조사하지말고) 시켜라!
 		}
 	}
 }
