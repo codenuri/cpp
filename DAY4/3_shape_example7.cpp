4ab8e849478de9d95019db8fa2108bca8d89ad18
ecourse <smkang@codenuri.co.kr>
2025-09-18 20:31:30 +0900

    Add files via upload

---
+++ DAY4/3_shape_example7.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+
+// 핵심 #6
+
+class Shape 
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+
+
+	void set_color(int c) { color = c;}
+	virtual int get_area() const { return 0;}
+	virtual void draw() { std::cout << "draw shape\n"; }
+};
+
+
+
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw rect\n"; }
+};
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw circle\n"; }
+};
+
+
+int main()
+{
+	std::vector<Shape*> v; 
+
+	while( 1 )
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if 		( cmd == 1 ) v.push_back(new Rect);
+		else if ( cmd == 2 ) v.push_back(new Circle);
+		else if ( cmd == 9 )
+		{
+			for ( auto s : v)
+				s->draw();
+		}
+		
+		// 이번 소스의 핵심
+		else if ( cmd == 8 )
+		{
+			std::cout << "몇번째 만들었던 도형을 복제 할까요 >> ";
+			int k;
+			std::cin >> k;
+
+			// k 번째 만들었던 도형과 완전히 동일한 도형을 만들어서 v에 추가합니다.
+			// 어떻게 해야 할까요 ?
+			// k 번째 도형은 어떤 도형일까요 ? 
+		}
+	}
+}
