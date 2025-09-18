e4b42d588198cbc5bf758cb46f5238a4559786ac
ecourse <smkang@codenuri.co.kr>
2025-09-18 21:09:25 +0900

    Add files via upload

---
+++ DAY4/3_shape_factory1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+
+
+class Shape 
+{
+	int color = 0;
+public:
+	virtual ~Shape() {}
+
+	void set_color(int c) { color = c;}
+	virtual int get_area() const { return 0;}
+	virtual void draw() { std::cout << "draw shape\n"; }
+
+	virtual Shape* clone() const {	return new Shape(*this); }
+};
+
+
+
+
+class Rect : public Shape
+{
+public:
+	void draw() override { std::cout << "draw rect\n"; }
+
+	Shape* clone() const override {	return new Rect(*this); }
+};
+
+
+class Circle : public Shape
+{
+public:
+	void draw() override { std::cout << "draw circle\n"; }
+
+	Shape* clone() const override {	return new Circle(*this); }
+};
+
+
+int main()
+{
+	std::vector<Shape*> v; 
+
+	while( 1 )
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if 		( cmd == 1 ) v.push_back(new Rect);
+		else if ( cmd == 2 ) v.push_back(new Circle);
+		else if ( cmd == 9 )
+		{
+			for ( auto s : v)
+				s->draw();	
+		}
+		else if ( cmd == 8 )
+		{
+			std::cout << "몇번째 만들었던 도형을 복제 할까요 >> ";
+			int k;
+			std::cin >> k;
+
+			v.push_back( v[k]->clone() ); 
+		}
+	}
+}
