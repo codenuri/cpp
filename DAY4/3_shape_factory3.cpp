79086c01388c50bd229bd0d9c4ce41be6d14647f
ecourse <smkang@codenuri.co.kr>
2025-09-18 21:47:40 +0900

    Add files via upload

--- DAY4/3_shape_factory3.cpp
+++ DAY4/3_shape_factory3.cpp
@@ -54,7 +54,7 @@ public:
 };
 
 
-// 아래 클래스는 "공장에 제품을 자동등록" 할때 사용할 클래스 입니다.
+// 아래 클래스는 "공장에 제품을 자동 등록" 할때 사용할 클래스 입니다.
 // 생성자만 사용합니다.
 class AutoRegister 
 {
@@ -66,19 +66,9 @@ public:
 };
 
 // 전역변수의 생성자가 호출되는 시점을 생각해 보세요
-
-AutoRegister ar{1, &Rect::create};
-
-
-
-
-
-
-
-
-
-
-
+// => main 함수가 호출되기 전에 생성자 실행
+// => 즉, 아래 코드가 있으면 main 함수가 시작되기 전에 이미 Rect 는 공장에 등록됨
+// AutoRegister ar{1, &Rect::create};
 
 
 class Rect : public Shape
@@ -87,8 +77,22 @@ public:
 	void draw() override { std::cout << "draw rect\n"; }
 
 	Shape* clone() const override {	return new Rect(*this); }
+
 	static Shape* create() { return new Rect;}
+
+	// static 멤버 데이타의 특징을 생각하세요
+	// => 객체가 없어도 메모리에 생성됨.
+	// => 즉, main 함수 이전에  ar의 생성자 호출됨
+	// => 따라서 공장에 Rect 등록됨.!
+	// [참고] C++17 의 inline static 사용하면 외부 정의 필요 없습니다.
+	inline static AutoRegister ar{1, &Rect::create};
 };
+
+// static 멤버 데이타는 클래스 외부에 아래 처럼 만들어야 하는데.
+// 결국 아래 코드가 전역변수 처럼 "Rect" 객체가 없어도 메모리에 생성되는것
+// AutoRegister Rect::ar{1, &Rect::create}; 
+
+
 
 
 
@@ -101,6 +105,8 @@ public:
 	Shape* clone() const override {	return new Circle(*this); }
 
 	static Shape* create() { return new Circle;}
+
+	inline static AutoRegister ar{2, &Circle::create};
 };
 
 
@@ -111,9 +117,9 @@ int main()
 
 	ShapeFactory& factory = ShapeFactory::get_instance();
 
-
-	factory.register_shape( 1, &Rect::create);
-	factory.register_shape( 2, &Circle::create);
+	// 아래 코드가 없어도 이미 도형은 공장에 등록되었습니다.
+//	factory.register_shape( 1, &Rect::create);
+//	factory.register_shape( 2, &Circle::create);
 
 
 	while( 1 )
