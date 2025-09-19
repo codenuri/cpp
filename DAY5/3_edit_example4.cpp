3390c03064c40e618fd8e7ebc01c719f38f42745
ecourse <smkang@codenuri.co.kr>
2025-09-19 20:24:07 +0900

    Add files via upload

--- DAY5/3_edit_example4.cpp
+++ DAY5/3_edit_example4.cpp
@@ -7,6 +7,24 @@
 // #1. 변하는 것을 "가상함수로"   - template method, edit3.cpp
 // #2. 변하는 것을 "다른클래스로" - strategy, 		  edit4.cpp
 
+// Validation 정책을 별도의 클래스로 분리 합니다.
+// => 그런데, 교체 가능해야 하므로 인터페이스를 먼저 설계 합니다.
+struct IValidator 
+{
+	virtual bool validate(const std::string& s, char c) = 0;
+	virtual bool is_complete(const std::string& s) { return true; }
+	virtual ~IValidator() {}
+};
+
+// 주민 등록 번호 : 910    3      확인
+
+
+
+
+
+
+
+
 class Edit
 {
 	std::string data;
