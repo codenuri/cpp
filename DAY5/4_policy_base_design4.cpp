a24140aa37978b7c9d424d403fbdbe9d34ecddf8
ecourse <smkang@codenuri.co.kr>
2025-09-19 21:41:35 +0900

    Add files via upload

--- DAY5/4_policy_base_design4.cpp
+++ DAY5/4_policy_base_design4.cpp
@@ -2,51 +2,40 @@
 
 // 방법 #3. policy base design
 
-template<typename T>
-struct IAllocator
-{
-	virtual T* allocate(std::size_t sz) = 0;
-	virtual void deallocate(T* ptr, std::size_t sz) = 0;
-	virtual ~IAllocator() {}
-};
 
-template<typename T>
+template<typename T, typename Alloc>
 class vector
 {
 	T* buff;
 	std::size_t size;
 
-	IAllocator<T>* alloc = nullptr;
+//	malloc_allocator<T> alloc; // 강한 결합. 교체 안됨.
+	Alloc alloc;			   // template 인자로 전달한 타입 사용. 즉, 템플릿 인자로 교체
 public:
-	void set_allocator(  IAllocator<T>* p) { alloc = p;}
-
 	void resize(int sz)
 	{
-		buff = alloc->allocate(sz); 
-
-		alloc->deallocate(ptr, sz);
+		buff = alloc.allocate(sz); 
+		alloc.deallocate(ptr, sz);
 	}
 };
 
 template<typename T>
-class malloc_allocator : public IAllocator<T>
+class malloc_allocator 
 {
 public:
-	T*   allocate(std::size_t sz) override 
+	inline T*   allocate(std::size_t sz) 
 	{ 
 		void* p = malloc(sizeof(T) * sz);
 		return static_cast<T*>(p);
 	}
-	void deallocate(T* ptr, std::size_t sz) override 
+	inline void deallocate(T* ptr, std::size_t sz)
 	{
 		free(ptr); 
 	}
 };
 int main()
 {	
-	vector<int> v;
-	malloc_allocator<int> ma;
-
-	v.set_allocator(&ma); 
+	vector<int, malloc_allocator<int>> v; // template 2번째 인자로 메모리 할당기 전달
+	
 	v.resize(10);
 }
