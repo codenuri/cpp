ac05ac1d61d1594eab95aba584f5cb132d06ab72
ecourse <smkang@codenuri.co.kr>
2025-09-19 19:23:53 +0900

    Add files via upload

--- DAY5/1_abstract_class2.cpp
+++ DAY5/1_abstract_class2.cpp
@@ -14,14 +14,16 @@ public:
 		std::cout << "mutex.unlock\n"; 
 	}
 
+	// 가상함수     : 파생 클래스가 override 하지 않으면 기본 구현 제공하겠다는 의도	
+	// 순수가상함수 : 파생 클래스가 반드시 override 해야 한다고 지시하는 것
+	//			    즉, "반드시 구현부을 제공해라" 라고 시키는 것
 protected:
-	virtual void draw_imp() 
-	{
-		std::cout << "draw shape\n";
-	}
+//	virtual void draw_imp() { std::cout << "draw shape\n"; 	}  // 나쁜 코드
+	virtual void draw_imp() = 0;	// 좋은 코드!
 
 public:
-	virtual Shape* clone() const {	return new Shape(*this); }
+//	virtual Shape* clone() const {	return new Shape(*this); }
+	virtual Shape* clone() const = 0;
 };
 
 
