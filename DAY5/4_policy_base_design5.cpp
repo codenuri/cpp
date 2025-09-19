2a9489646f1fdcbb38bc849fd7851986f20a16ab
ecourse <smkang@codenuri.co.kr>
2025-09-19 21:52:30 +0900

    Add files via upload

--- DAY5/4_policy_base_design5.cpp
+++ DAY5/4_policy_base_design5.cpp
@@ -19,6 +19,15 @@ public:
 		printf("[deallocate] : %p, %u\n", ptr, sz);
 		free(ptr); 
 	}
+
+	// 사용자 정의 메모리 할당기를 만들려면 미리 정의된 약속을 지켜야 합니다.
+	// 위 2개 함수와 
+	// 아래 3개  <== 관례적인 코드.. 항상 아래 처럼만 만들면 됩니다.
+
+	using value_type = T;
+	debug_alloc() {}  // 디폴트 생성자
+	template<typename U> debug_alloc( const debug_alloc<U>& ){} // generic 생성자 - 약간 어려운 개념.
+																// 하지만 항상 이렇게만 하면됩니다.
 };
 
 
