f0ec3077bc938a24cb074324569069a20b38077c
ecourse <smkang@codenuri.co.kr>
2025-09-29 21:35:11 +0900

    Add files via upload

--- DAY6/4_move07-2.cpp
+++ DAY6/4_move07-2.cpp
@@ -2,6 +2,10 @@
 #include <string>
 
 // 규칙 #3. 사용자가 이동 계열만 제공할때
+// => 컴파일러는 복사 계열 제공 안함
+
+// 복사생성자가 필요한 코드가 있다면
+// => 컴파일 에러
 
 struct Object
 {
@@ -10,13 +14,12 @@ struct Object
 
 	Object(Object&& obj) : name{std::move(obj.name)} { std::cout << "user move\n";}
 
-
-	// 이때 컴파일러에게 이동생성자의 디폴트 버전을 요청하려면 = default 문법 사용
-//	Object(Object&&) = default;
+	// 이때 컴파일러에게 복사생성자의 디폴트 버전을 요청하려면 = default 문법 사용
+	Object(const Object&) = default;
 
 	// 대입 2개도 모두 디폴트 요청하는 것이 관례
-//	Object& operator=(const Object&) = default;
-//	Object& operator=(Object&&) = default;
+	Object& operator=(const Object&) = default;
+	Object& operator=(Object&&) = default;
 };
 
 int main()
@@ -24,8 +27,8 @@ int main()
 	Object o1{"obj1"};
 	Object o2{"obj2"};
 
-	Object o3 = o1;				
-	Object o4 = std::move(o1);	
+	Object o3 = o1;				// error. 	단, =default로 요청했다면 ok 
+	Object o4 = std::move(o1);	// "사용자가 만든 move 생성자 호출"
 								
 
 	std::cout << o1.name << std::endl; 
