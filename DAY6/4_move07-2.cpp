0a1ed2aa0b61c73da39b8721901bfcc7384674b4
ecourse <smkang@codenuri.co.kr>
2025-09-29 21:31:45 +0900

    Add files via upload

---
+++ DAY6/4_move07-2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+// 규칙 #3. 사용자가 이동 계열만 제공할때
+
+struct Object
+{
+	std::string name;
+	Object(const std::string& n) : name(n) {}	
+
+	Object(Object&& obj) : name{std::move(obj.name)} { std::cout << "user move\n";}
+
+
+	// 이때 컴파일러에게 이동생성자의 디폴트 버전을 요청하려면 = default 문법 사용
+//	Object(Object&&) = default;
+
+	// 대입 2개도 모두 디폴트 요청하는 것이 관례
+//	Object& operator=(const Object&) = default;
+//	Object& operator=(Object&&) = default;
+};
+
+int main()
+{
+	Object o1{"obj1"};
+	Object o2{"obj2"};
+
+	Object o3 = o1;				
+	Object o4 = std::move(o1);	
+								
+
+	std::cout << o1.name << std::endl; 
+	std::cout << o2.name << std::endl; 
+}
