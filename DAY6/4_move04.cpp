de28a0755d24628021af1dcae8e9576a8b197652
ecourse <smkang@codenuri.co.kr>
2025-09-29 20:41:30 +0900

    Add files via upload

--- DAY6/4_move04.cpp
+++ DAY6/4_move04.cpp
@@ -15,13 +15,28 @@ public:
 	}
 	~Cat() { delete[] name; }
 
+	// 일반적인 복사 생성자
+	// => "깊은 복사로 구현"
 	Cat(const Cat& c) : age(c.age)
 	{
 		name = new char[strlen(c.name) + 1];
 		strcpy_s(name, strlen(c.name) + 1, c.name);
+	}
+
+	// temporary 를 위한 복사 생성자
+	// => 인자로 Cat&& 사용
+	// => 이동(move) 기술.
+	Cat(Cat&& c) : name{c.name}, age{c.age}
+	{
+		// temporary 가 자원을 delete 할수 없도록
+		// 자원 포인터를 reset
+		// => 아래 코드를 실행하려면 "상수성 없이 temporary를 가리킬수 있어야 했다."
+		// => 그래서 rvalue reference 는 상수성 없이 temporary 가리키는 것이 가능. 
+		c.name = nullptr;
 	}	
 };
 
+// #1. foo() 의 값환 값은 "temporary" 입니다.
 Cat foo()
 {
 	Cat c{"yaong", 3};
