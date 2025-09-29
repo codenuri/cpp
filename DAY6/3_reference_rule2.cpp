d38f696b779165cd3ea4d055ed4f99b2a8f44de3
ecourse <smkang@codenuri.co.kr>
2025-09-29 19:55:29 +0900

    Add files via upload

---
+++ DAY6/3_reference_rule2.cpp
@@ -0,0 +1,25 @@
+struct Point 
+{ 
+	int x;
+	int y; 
+};
+// C++ reference 규칙.
+// => 너무 중요합니다. 반드시 외워야 합니다.
+int main()
+{
+	Point pt{1, 2};
+
+	// #1. non-const reference 는 lvalue 만 가리킬수 있다.
+	Point& r1 = pt;					// ok.
+	Point& r2 = Point{1, 2};		// error.
+
+	// #2. const reference 는 lvalue 와 rvalue 를 모두 가리킬수 있다.
+	const Point& r3 = pt;			// ok
+	const Point& r4 = Point{1, 2};	// ok - 되는 이유는 1번소스 참고. 
+
+	// C++98 시절은 위 2개의 규칙만 존재
+	// C++11 에서 move 와 perfect forwarding 을 만들기 위해 새로운 문법 도입
+	// #3. rvalue reference 는 rvalue 만 가르킬수 있다.
+	Point&& r5 = pt;         // error
+	Point&& r6 = Point{1,2}; // ok
+}
