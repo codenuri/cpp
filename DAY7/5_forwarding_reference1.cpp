0f5f4da22e15f1dd5a9998a9df231c60cc480a5b
ecourse <smkang@codenuri.co.kr>
2025-09-30 20:51:01 +0900

    Add files via upload

---
+++ DAY7/5_forwarding_reference1.cpp
@@ -0,0 +1,14 @@
+
+// forwarding reference 란 ?
+// => 함수 템플릿 인자로 사용되는 T&&
+// => C++11 의 핵심 문법
+// => 모든 C++ 개발자가 반드시 알아야 하는 핵심 문법
+
+template<typename T> void chronomety(T&& a)
+{
+}
+
+int main()
+{
+
+}
