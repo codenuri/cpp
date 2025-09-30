ad0ddb4a57372895080392124b97f42de9a58039
ecourse <smkang@codenuri.co.kr>
2025-09-30 20:54:27 +0900

    Add files via upload

---
+++ DAY7/5_forwarding_reference2.cpp
@@ -0,0 +1,34 @@
+// 사전 지식. 
+int main()
+{
+	int n = 10;
+
+	int *p = &n;
+	int* *pp = &p; // ok. 포인터를 가리키는 포인터는 만들수 있습니다.
+					//    이중 포인터 
+
+	int& r = n;
+	int& &rr = r;	// error. 핵심 #1 : reference 를 가리키는 reference 는 만들수 없습니다.
+					
+
+	using LREF = int&; 
+	using RREF = int&&;
+	// 핵심 #2. reference to reference 를 개발자가 직접 코드로 만들수 없지만
+	//			type alias 사용시 또는 타입 추론시에 발생되는 reference to reference 는 
+	//          에러가 아니고, "reference collapsing" 규칙 으로 해석 됩니다.
+	// reference collapsing
+	LREF& r3 = ? ;
+	RREF& r4 = ? ;
+	LREF&& r5 = ? ;
+	RREF&& r6 = ?;
+}
+
+template<typename T> void foo(T a)
+{
+	T& r = a;
+}
+
+
+
+
+
